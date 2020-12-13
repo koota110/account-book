897206e71d4393456004a11696ba352204606a03
koota110 <es.inest21@gmail.com>
2020-12-13 15:07:46 +0900

    initial commit

---
+++ pages/budgets/list.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-data-table
+    :headers="headers">
+  </v-data-table>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      headers:[
+        {
+          text:'Name',
+          value:'name'
+        }
+      ]
+    }
+  }
+})
+</script>
